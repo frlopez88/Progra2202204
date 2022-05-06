be962e9fa32d1c6cffdc76f8d74e08133049c3ce
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-05 19:27:41 -0600

    Update Objetivos.cs

--- Semana2Sesion1/Objetivos.cs
+++ Semana2Sesion1/Objetivos.cs
@@ -69,7 +69,7 @@ ademas la persona puede llenar el tanque de gasolina mandando el parametro de ga
 -- Problema a Resolver
 
 1) Crear una lista de Clientes
-con 10 registros 
+con 10 registros.
 2) Recorrer la lista de clientes y ponerlos a correr(20 kms)
 3) Recorrer la lista de clientes y generar reporte de GetReporteContador()
 
